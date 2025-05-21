2dc4ea293ea8b3aa68ab53eaba7500d1c3e8529f
IskenderDev <iskenderkadyrjanov@gmail.com>
2025-05-21 13:32:11 +0600

    dev

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ export const Home = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold">{t('welcome')}</h1>
-      <p>{t('description')}</p>
+      <p>{t('description')}d</p>
     </div>
   );
 };
